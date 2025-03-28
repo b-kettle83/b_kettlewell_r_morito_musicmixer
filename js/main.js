7d96f019db058b08a345035fe517a44c548e9064
b-kettle83 <kettben832@gmail.com>
2025-03-28 18:05:07 -0400

    Add Drag & Drop
    This commit adds the drag and drop functionality to the build using placeholder images

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
 console.log("JS file connected");
+
+const resetButton = document.querySelector('#audios button');
+
+let audios = document.querySelectorAll('#audios img'),
+    dropZones = document.querySelectorAll('.drop_zone'),
+    originalContainer = document.querySelector('#audios'),
+    originalZone,
+    zoneTaken,
+    draggedPiece;
+
+    dropZones.forEach(zone => zone.zoneTaken = false);
+
+function resetAudios() {
+    audios.forEach(audio => originalContainer.appendChild(audio));
+
+    dropZones.forEach(zone => zone.zoneTaken = false);
+}
+
+function startDrag () {
+    console.log ('Started dragging ', this.id);
+
+    draggedPiece = this;
+
+    originalZone = this.parentElement;
+}
+
+function handleDragOver (e) {
+    e.preventDefault();
+    console.log('Dragged Over');
+}
+
+function handleDrop (e) {
+    e.preventDefault();
+
+    if (this.zoneTaken === true) {
+        console.log('Zone Taken');
+        return;
+    }
+
+    this.appendChild(draggedPiece);
+    console.log('Audio Dropped');
+    this.zoneTaken = true;
+
+    originalZone.zoneTaken = false;
+}
+
+resetButton.addEventListener('click', resetAudios);
+
+audios.forEach(audio => audio.addEventListener('dragstart', startDrag));
+
+dropZones.forEach(zone => zone.addEventListener('dragover', handleDragOver));
+
+dropZones.forEach(zone => zone.addEventListener('drop', handleDrop));
